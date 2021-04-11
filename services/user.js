aa6dac3ce51b5af9fd589cf65b20e2fcc8f97135
Mayur <mayuragarwal2812@gmail.com>
2021-04-11 14:43:58 +0530

    commit

--- services/user.js
+++ services/user.js
@@ -3,10 +3,22 @@ const model = require("../models/user");
 
 let userService = {};
 
-userService.addtoOrder = async(product) => {
-  return await model.addtoOrder(product);
+userService.addtoOrder = async (product) => {
+  if (validator.addtoOrder(product)) return await model.addtoOrder(product);
+  else {
+    let err = new Error();
+    err.status = 400;
+    err.message = "Item not found,Please check the item and try again";
+    throw err;
+  }
 };
-userService.getfromOrder = async(userid) => {
-  return await model.getfromOrder(userid);
+userService.getfromOrder = async (userid) => {
+  if (validator.getfromOrder(userid)) return await model.getfromOrder(userid);
+  else {
+    let err = new Error();
+    err.status = 400;
+    err.message = "Item not found,Please check the item and try again";
+    throw err;
+  }
 };
 module.exports = userService;
